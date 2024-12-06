b5c91b749b0fb328bdfad9b652c6116c55743791
fabriciogomesdev <fabriciogomesdev@gmail.com>
2024-12-06 17:05:47 -0300

    Primeiro Commit

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { Routes, Route} from 'react-router-dom'
+import { Home } from './pages/home/index';
+import { History } from './pages/history/index';
+import { DefaultLayout } from './layouts/DefaultLayout/Index';
+
+export function Router(){
+    return (
+        <Routes>
+            <Route path='/' element={<DefaultLayout/>}>
+                <Route path='/' element={<Home />}/>
+                <Route path='/history' element={<History />}/>
+            </Route>  
+        </Routes>
+    );
+}
