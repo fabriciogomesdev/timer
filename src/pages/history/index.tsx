10982c2ffc5198da9c8a57de9f483fc596104c1a
Fabricio <fabriciogomesdev@gmail.com>
2024-12-09 14:37:35 -0300

    atualizacoes

--- src/pages/history/index.tsx
+++ src/pages/history/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History(){
     return (
@@ -21,31 +21,41 @@ export function History(){
                         <td>Tarefa</td>
                         <td>20 minutos</td>
                         <td>Há dois meses</td>
-                        <td>Concluido</td>
+                        <td>
+                            <Status statusColor = "green">Concluído</Status>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Tarefa</td>
+                        <td>20 minutos</td>
+                        <td>Há dois meses</td>                 
+                        <td>
+                            <Status statusColor = "green">Concluído</Status>
+                        </td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 minutos</td>
                         <td>Há dois meses</td>
-                        <td>Concluido</td>
+                        <td>
+                            <Status statusColor = "green">Concluído</Status>
+                        </td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 minutos</td>
                         <td>Há dois meses</td>
-                        <td>Concluido</td>
+                        <td>
+                            <Status statusColor = "red">Interrompido</Status>
+                        </td>
                     </tr>
                     <tr>
                         <td>Tarefa</td>
                         <td>20 minutos</td>
                         <td>Há dois meses</td>
-                        <td>Concluido</td>
-                    </tr>
-                    <tr>
-                        <td>Tarefa</td>
-                        <td>20 minutos</td>
-                        <td>Há dois meses</td>
-                        <td>Concluido</td>
+                        <td>
+                            <Status statusColor = "yellow">Em andamento</Status>
+                        </td>
                     </tr>
                 </tbody>
                 </table>
